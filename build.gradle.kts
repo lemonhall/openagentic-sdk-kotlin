c9e01fc907e3e8f2547292a710358579da8fa001
Lemon Hall <lemonhall@gmail.com>
2026-02-21 00:41:30 +0800

    build: Kotlin 2.0.21
    Bump wrapper to Gradle 8.9 for AGP compatibility.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
     application
 }
 
